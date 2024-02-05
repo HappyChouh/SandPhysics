81500121aa82cd3162832ad343db85781516b61f
RonanBalusson <balusson.ronan16@gmail.com>
2024-02-05 23:24:31 +0100

    feat/getting_started_with_the_framework

--- SandPhysics/Game1.cs
+++ SandPhysics/Game1.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SandPhysics.Physics;
+using SharpDX.Direct3D9;
 
 namespace SandPhysics
 {
     public class Game1 : Game
     {
+        private Texture2D grainOfSand;
+        private Vector2 grainOfSandPosition;
+        private float grainOfSandSpeed;
+
+        private Texture2D ground;
+
+        private PhysicsOfSand physics; 
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -19,6 +29,10 @@ namespace SandPhysics
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            grainOfSandPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+            grainOfSandSpeed = 0.5f;
+
+            physics = new PhysicsOfSand();
 
             base.Initialize();
         }
@@ -28,6 +42,8 @@ namespace SandPhysics
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            grainOfSand = Content.Load<Texture2D>("sand");
+            ground = Content.Load<Texture2D>("ground");
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,8 +51,16 @@ namespace SandPhysics
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-
+            // TODO: Add your update logic 
+            grainOfSandSpeed = grainOfSandSpeed + (grainOfSandSpeed * 0.3f);
+            if (grainOfSandPosition.Y + grainOfSandSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds > 480)
+            {
+                grainOfSandPosition.Y = 470;
+            }
+            else
+            {
+                grainOfSandPosition.Y = physics.GetYPosition(grainOfSandPosition.Y, grainOfSandSpeed, gameTime);
+            }
             base.Update(gameTime);
         }
 
@@ -45,6 +69,11 @@ namespace SandPhysics
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(ground, new Vector2(0, 480), Color.White);
+            _spriteBatch.Draw(grainOfSand, grainOfSandPosition, null, Color.White, 0f, new Vector2(grainOfSand.Width / 2, grainOfSand.Height / 2), Vector2.One, SpriteEffects.None, 0f);
+            _spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
---
+++ SandPhysics/Physics/PhysicsOfSand.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace SandPhysics.Physics
+{
+    public class PhysicsOfSand
+    {
+        public PhysicsOfSand()
+        {
+
+        }
+
+        public float GetYPosition(float grainOfSandPositionY, float grainOfSandSpeed, GameTime gameTime)
+        {
+            return grainOfSandPositionY += grainOfSandSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+    }
+}
